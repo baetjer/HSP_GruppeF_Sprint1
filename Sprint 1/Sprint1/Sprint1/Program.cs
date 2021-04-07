575503961fce2487c5ee32d722e8d6cc60db2518
O.Bätjer <79996046+baetjer@users.noreply.github.com>
2021-04-07 19:09:46 +0200

    Create Program.cs

---
+++ Sprint 1/Sprint1/Sprint1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint1
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
