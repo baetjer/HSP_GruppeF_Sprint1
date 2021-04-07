d9512ae51d84276c1676aade0ca995a5ae597af4
O.Bätjer <79996046+baetjer@users.noreply.github.com>
2021-04-07 19:27:14 +0200

    Create Schraubenkopf.cs

---
+++ Sprint 1/Sprint1/Sprint1/Schraubenkopf.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint1
+{
+    public class Schraubenkopf
+    {
+        string innensechskant = "Innensechskant";
+
+        public string Innensechskant { get => innensechskant; set => innensechskant = value; }
+    }
+}
