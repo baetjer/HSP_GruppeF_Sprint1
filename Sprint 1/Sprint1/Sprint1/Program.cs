ec05ed09c7124d459fda164a840a263fe9057d8b
O.Bätjer <79996046+baetjer@users.noreply.github.com>
2021-04-08 15:51:30 +0200

    Update Program.cs

--- Sprint 1/Sprint1/Sprint1/Program.cs
+++ Sprint 1/Sprint1/Sprint1/Program.cs
@@ -10,22 +10,9 @@ namespace Sprint1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Bitte wählen Sie einen Schraubenkopf aus:");
-
             Schraubenkopf sk = new Schraubenkopf();
             sk.Schraubenkopf_Array();
-
-            int input_schraubenkopf = Convert.ToInt32(Console.ReadLine());
-
-            while ((input_schraubenkopf != 0) && (input_schraubenkopf != 1))
-            {
-                Console.WriteLine("Falsche Eingabe, bitte wählen Sie nochmal.");
-                input_schraubenkopf = Convert.ToInt32(Console.ReadLine());
-            }
-
-            string output_schraubenkopf = sk.getSchraubenkopf_Array(input_schraubenkopf);
-
-            Console.WriteLine("Sie haben sich für {0} entschieden.", output_schraubenkopf);
+            sk.Schraubenkopf_Ausgabe();
 
 
 
