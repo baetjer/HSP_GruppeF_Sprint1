fde878bd338feb799891973658a5a9f986b96af0
timwurbs <tim.wurbs@student.jade-hs.de>
2021-04-08 18:47:09 +0200

    Create Schraubenschaft.cs

---
+++ Sprint 1/Sprint1/Sprint1/Schraubenschaft.cs
@@ -0,0 +1,40 @@
+﻿namespace Sprint1
+{
+    class Schraubenschaft
+    {
+        int schraubenschaft_zahl1;
+
+        string[] schraubenschaft_array = new string[5] { "=10mm", "=16mm", "=20mm", "=30mm", "=40mm" };
+
+        public void Schraubenschaft_Array()
+        {
+            console.WriteLine("Bitte wählen Sie eine Schaftlänge aus:");
+
+            foreach (string i in schraubenschaft_array)
+            {
+                console.WriteLine(schraubenschaft_zahl1 + " " + i);
+                schraubenschaft_zahl1 += 1;
+            }
+        }
+        
+        public string getSchraubenschaft_Array(int i)
+        {
+            return schraubenschaft_array[i];
+        }
+        
+        public void Schraubenschaft_Ausgabe()
+        {
+            int input_schraubenschaft = Convert.ToInt32(Console.ReadLine());
+
+            while ((input_schraubenschaft < 0) && (input_schraubenschaft > 4))
+            {
+                Console.WriteLine("Falsche Eingabe, bitte wählen Sie nochmal.");
+                input_schraubenschaft = Convert.ToInt32(Console.ReadLine());
+            }
+
+            string output_schraubenschaft = getSchraubenschaft_Array(input_schraubenschaft);
+
+            Console.WriteLine("Sie haben sich für die Länge {0} entschieden.", output_schraubenschaft);
+        }
+    }
+}
