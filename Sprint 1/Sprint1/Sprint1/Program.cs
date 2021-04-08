c479e876489e18f1b2f05eba967becb827c00ef3
O.Bätjer <79996046+baetjer@users.noreply.github.com>
2021-04-08 14:37:57 +0200

    while Schleife

--- Sprint 1/Sprint1/Sprint1/Program.cs
+++ Sprint 1/Sprint1/Sprint1/Program.cs
@@ -16,6 +16,13 @@ namespace Sprint1
             sk.Schraubenkopf_Array();
 
             int input_schraubenkopf = Convert.ToInt32(Console.ReadLine());
+
+            while ((input_schraubenkopf != 0) && (input_schraubenkopf != 1))
+            {
+                Console.WriteLine("Falsche Eingabe, bitte wählen Sie nochmal.");
+                input_schraubenkopf = Convert.ToInt32(Console.ReadLine());
+            }
+
             string output_schraubenkopf = sk.getSchraubenkopf_Array(input_schraubenkopf);
 
             Console.WriteLine("Sie haben sich für {0} entschieden.", output_schraubenkopf);
