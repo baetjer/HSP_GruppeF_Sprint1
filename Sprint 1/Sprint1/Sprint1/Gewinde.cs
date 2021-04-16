4f397ea3bca316aa1cc06b14c08f4e26791769ef
O.Bätjer <79996046+baetjer@users.noreply.github.com>
2021-04-16 11:16:42 +0200

    Update Gewinde.cs

--- Sprint 1/Sprint1/Sprint1/Gewinde.cs
+++ Sprint 1/Sprint1/Sprint1/Gewinde.cs
@@ -9,25 +9,24 @@ namespace Sprint1
     class Gewinde
     {
         int input_gewinde = 0;
-        string output_gewinde;
+        int output_gewinde;
         int gewinde_zahl1;
 
         //string[,] gewinde_array2D = new string[,] { {"2mm","3mm"}, { "3.5mm", "4mm" }, { "4.5mm", "5mm" }, { "5.5mm", "6mm" }, { "6.5mm", "7mm" } };
-        string[] gewinde_array = new string[10] { "M 3","3mm",  "3.5mm", "4mm" ,  "4.5mm", "5mm" ,  "5.5mm", "6mm" , "6.5mm", "7mm" };
-        int[] gewinde_daten = new int[] { 3, 4, 5};
+        int[] gewinde_array = new int[12] {3, 4, 5, 6, 8, 10, 12, 14, 16, 20, 24, 30};
 
         public void Gewinde_Array()
         {
             Console.WriteLine("Bitte wählen Sie ein Gewinde aus:");
 
-            foreach (string i in gewinde_array)
+            foreach (int i in gewinde_array)
             {
-                Console.WriteLine(gewinde_zahl1 + " = " + i);
+                Console.WriteLine(gewinde_zahl1 + " = " + "M{0}", i);
                 gewinde_zahl1 += 1;
             }
         }
 
-        public string getGewinde_Array(int i)
+        public int getGewinde_Array(int i)
         {
             return gewinde_array[i];
         }
@@ -36,7 +35,7 @@ namespace Sprint1
         {
             input_gewinde = Convert.ToInt32(Console.ReadLine());
 
-            while (input_gewinde < 0 || input_gewinde > 9)
+            while (input_gewinde < 0 || input_gewinde > 11)
             {
                 Console.WriteLine("Falsche Eingabe, bitte wählen Sie nochmal.");
                 input_gewinde = Convert.ToInt32(Console.ReadLine());
@@ -45,13 +44,13 @@ namespace Sprint1
             output_gewinde = getGewinde_Array(input_gewinde);
 
             Console.WriteLine("--------------------------------------------------");
-            Console.WriteLine("Sie haben sich für den Durchmesser {0} entschieden.", output_gewinde);
+            Console.WriteLine("Sie haben sich für M{0} entschieden.", output_gewinde);
             Console.WriteLine("--------------------------------------------------");
         }
 
         public void Gewinde_Warenkorb()
         {
-            Console.WriteLine("Gewindedurchmesser: " + output_gewinde);
+            Console.WriteLine("Gewindedurchmesser: M{0}", output_gewinde);
         }
     }
 }
