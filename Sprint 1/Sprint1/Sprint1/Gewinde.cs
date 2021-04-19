45eaaa7261e88dd32d02ed69e58b6cd5721cd2c6
O.Bätjer <79996046+baetjer@users.noreply.github.com>
2021-04-19 14:18:15 +0200

    Update Gewinde.cs

--- Sprint 1/Sprint1/Sprint1/Gewinde.cs
+++ Sprint 1/Sprint1/Sprint1/Gewinde.cs
@@ -8,6 +8,7 @@ namespace Sprint1
 {
     class Gewinde
     {
+        //Gewinde
         int input_gewinde = 0;
         int output_gewinde = 0;
         int gewinde_zahl1 = 0;
@@ -122,6 +123,123 @@ namespace Sprint1
             else
             {
 
+            }
+        }
+
+        //Schraubenkopf
+        int input_schraubenkopf = 0;
+        string output_schraubenkopf;
+        int schraubenkopf_zahl1 = 0;
+
+        string[] schraubenkopf_array = new string[2] { "Innensechskant", "Sechskant" };
+
+        public void Schraubenkopf_Array()
+        {
+            Console.WriteLine("Bitte wählen Sie einen Schraubenkopf aus:");
+
+            foreach (string i in schraubenkopf_array)
+            {
+                Console.WriteLine(schraubenkopf_zahl1 + " = " + i);
+                schraubenkopf_zahl1++;
+            }
+        }
+
+        public string getSchraubenkopf_Array(int i)
+        {
+            return schraubenkopf_array[i];
+        }
+
+        public void Schraubenkopf_Ausgabe()
+        {
+            input_schraubenkopf = Convert.ToInt32(Console.ReadLine());
+
+            while ((input_schraubenkopf != 0) && (input_schraubenkopf != 1))
+            {
+                Console.WriteLine("Falsche Eingabe, bitte wählen Sie nochmal.");
+                input_schraubenkopf = Convert.ToInt32(Console.ReadLine());
+            }
+
+            output_schraubenkopf = getSchraubenkopf_Array(input_schraubenkopf);
+
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("Sie haben sich für {0} entschieden.", output_schraubenkopf);
+            Console.WriteLine("--------------------------------------------------");
+        }
+
+
+        //Schraubenkopf-Werte
+        double output2_Schraubenkopf;
+        double output3_Schraubenkopf;
+        double output4_Schraubenkopf;
+        double output5_Schraubenkopf;
+
+        //Sechskant
+        double[] s_sk = new double[12] { 5.5, 7, 8, 10, 13, 16, 18, 21, 24, 30, 36, 46 };
+        double[] k_sk = new double[12] { 2, 2.8, 3.5, 4, 5.3, 6.4, 7.5, 8.8, 10, 12.5, 15, 18.7 };
+
+        //Innensechskant
+        double[] d1_is = new double[12] { 5.5, 7, 8.5, 10, 13, 16, 18, 21, 24, 30, 36, 45 };
+        double[] k_is = new double[12] { 3, 4, 5, 6, 8, 10, 12, 14, 16, 20, 24, 30 };
+        double[] s_is = new double[12] { 2.5, 3, 4, 5, 6, 8, 10, 12, 14, 17, 19, 22 };
+        double[] dh_is = new double[12] { 3.5, 4.5, 5.5, 6.6, 9, 11, 13.5, 15.5, 17.5, 22, 26, 33 };
+
+        public double getS_SK(int i)
+        {
+            return s_sk[i];
+        }
+
+        public double getK_SK(int i)
+        {
+            return k_sk[i];
+        }
+
+        public double getD1_IS(int i)
+        {
+            return d1_is[i];
+        }
+
+        public double getK_IS(int i)
+        {
+            return k_is[i];
+        }
+
+        public double getS_IS(int i)
+        {
+            return s_is[i];
+        }
+
+        public double getDh_IS(int i)
+        {
+            return dh_is[i];
+        }
+
+        public void Schraubenkopf_Erzeugung()
+        {
+            if (input_schraubenkopf == 0)
+            {
+                output2_Schraubenkopf = getD1_IS(input_gewinde);
+                output3_Schraubenkopf = getK_IS(input_gewinde);
+                output4_Schraubenkopf = getS_IS(input_gewinde);
+                output5_Schraubenkopf = getDh_IS(input_gewinde);
+            }
+
+            else if (input_schraubenkopf == 1)
+            {
+                output2_Schraubenkopf = getS_SK(input_gewinde);
+                output3_Schraubenkopf = getK_SK(input_gewinde);
+            }
+        }
+
+        public void Schraubenkopf_Warenkorb()
+        {
+            if (input_schraubenkopf == 0)
+            {
+                Console.WriteLine("{0} | d1 = {1} | k = {2} | s = {3} | dh = {4}", output_schraubenkopf, output2_Schraubenkopf, output3_Schraubenkopf, output4_Schraubenkopf, output5_Schraubenkopf);
+            }
+
+            else if (input_schraubenkopf == 1)
+            {
+                Console.WriteLine("{0} | s = {1} | k = {2}", output_schraubenkopf, output2_Schraubenkopf, output3_Schraubenkopf);
             }
         }
     }
