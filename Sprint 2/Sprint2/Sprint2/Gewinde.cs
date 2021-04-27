e586b95b2b7f02ad84e19c295f807e427a2fd701
O.Bätjer <79996046+baetjer@users.noreply.github.com>
2021-04-27 18:36:15 +0200

    Update Gewinde.cs

--- Sprint 2/Sprint2/Sprint2/Gewinde.cs
+++ Sprint 2/Sprint2/Sprint2/Gewinde.cs
@@ -8,5 +8,32 @@ namespace Sprint2
 {
     class Gewinde
     {
+        int input_gewinde = 0;
+
+
+        public void test_gewinde()
+        {
+            try
+            {
+                input_gewinde = Convert.ToInt32(Console.ReadLine());
+                while ((input_gewinde != 0) && (input_gewinde != 1))
+                {
+                    Console.WriteLine("Falsche Eingabe, bitte wählen Sie nochmal.");
+                    input_gewinde = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Falsche Eingabe");
+                test_gewinde();
+            }
+        }
+
+        public int getInput_Gewinde()
+        {
+            return input_gewinde;
+        }
+
+        
     }
 }
