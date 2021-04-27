02769db48efef973fed6c2b35b94e4864b1ddcb4
O.Bätjer <79996046+baetjer@users.noreply.github.com>
2021-04-27 15:16:33 +0200

    Create Schraubenkopf.cs

---
+++ Sprint 2/Sprint2/Sprint2/Schraubenkopf.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint2
+{
+    class Schraubenkopf
+    {
+        string[] schraubenkopf_array = new string[3] { "Sechskantschraube", "Zylinderschraube", "Senkschraube"};
+        string[] zylinderschraube_array = new string[2] { "Innensechskant", "Schlitz" };
+
+
+
+    }
+}
