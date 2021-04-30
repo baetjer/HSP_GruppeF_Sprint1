a540d02aec0ce84bf3c7fe8c54d262551e43a827
O.Bätjer <79996046+baetjer@users.noreply.github.com>
2021-04-30 16:13:47 +0200

    Create ExcelControl.cs

---
+++ Sprint 2/Sprint2/Sprint2/ExcelControl.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Sprint2
+{
+    class ExcelControl
+    {
+        public ExcelControl()
+        {
+            Excel.Application excelAPP = new Excel.Application();
+            excelAPP.Visible = true;
+            excelAPP.Workbooks.Add();
+
+            Excel._Worksheet mySheet = (Excel.Worksheet) excelAPP.ActiveSheet;
+            mySheet.Cells[1, "A"] = "Hallo Welt";
+
+            var workSheet_range = mySheet.get_Range("A1", "B3");
+            workSheet_range.Borders[Excel.XlBordersIndex.xlEdgeBottom].Color = ConsoleColor.Green;
+        }
+    }
+}
