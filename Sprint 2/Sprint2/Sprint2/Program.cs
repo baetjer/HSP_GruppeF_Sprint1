77ba5de4c544970c4300391569a0f31427a4abc9
O.Bätjer <79996046+baetjer@users.noreply.github.com>
2021-04-30 16:13:49 +0200

    Update Program.cs

--- Sprint 2/Sprint2/Sprint2/Program.cs
+++ Sprint 2/Sprint2/Sprint2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Sprint2
 {
@@ -10,6 +11,7 @@ namespace Sprint2
     {
         static void Main(string[] args)
         {
+            new ExcelControl();
             Gewinde gw = new Gewinde();
             Schraubenkopf sk = new Schraubenkopf(gw);
 
