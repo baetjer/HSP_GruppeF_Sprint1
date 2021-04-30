4385d01b60030e2657d8b7985b6246b26ed903cd
O.Bätjer <79996046+baetjer@users.noreply.github.com>
2021-04-30 16:13:42 +0200

    Update ExcelControl.cs

--- Sprint 2/Sprint2 GUI/Sprint2 GUI/ExcelControl.cs
+++ Sprint 2/Sprint2 GUI/Sprint2 GUI/ExcelControl.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
 
 namespace Sprint2_GUI
 {
@@ -10,13 +11,10 @@ namespace Sprint2_GUI
     {
         ExcelControl()
         {
+            Application excelAPP = new Application();
+            excelAPP.Visible = true;
+            excelAPP.Workbooks.Add();
 
         }
-
-        static void Main()
-        {
-
-        }
-            
     }
 }
