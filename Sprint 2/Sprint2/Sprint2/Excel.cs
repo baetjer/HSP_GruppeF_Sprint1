e787d0922fc58e5e40ab91012d81c6b9ca5916f1
O.Bätjer <79996046+baetjer@users.noreply.github.com>
2021-05-02 22:05:06 +0200

    Create Excel.cs

---
+++ Sprint 2/Sprint2/Sprint2/Excel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
+
+namespace Sprint2
+{
+    class Excel
+    {
+        string path = "";
+        
+        Workbook wb;
+        Worksheet ws;
+
+        public Excel(string path, int sheet)
+        {
+            _Application excel = new _Excel.Application();
+            this.path = path;
+            Console.WriteLine(this.path);
+            Console.ReadKey();
+            wb = excel.Workbooks.Open(path);
+            ws = wb.Worksheets[sheet];
+
+        }
+
+        public string ReadCell(int i, int j)
+        {
+            i++;
+            j++;
+            if (ws.Cells[i, j].Value2 != null)
+                return ws.Cells[i, j].Value2;
+            else
+                return "";
+        }
+    }
+}
