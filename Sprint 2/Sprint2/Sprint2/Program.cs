3093f397add264ab57ae20530fac1b1436bf195f
O.Bätjer <79996046+baetjer@users.noreply.github.com>
2021-05-04 10:43:42 +0200

    Update Program.cs

--- Sprint 2/Sprint2/Sprint2/Program.cs
+++ Sprint 2/Sprint2/Sprint2/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Excel = Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Interop.Excel;
 using _Excel = Microsoft.Office.Interop.Excel;
 
 namespace Sprint2
