d02268ce13a033efa195b335b0eabf406ad60bbd
O.Bätjer <79996046+baetjer@users.noreply.github.com>
2021-05-09 13:09:15 +0200

    Update Berechnungen.cs

--- Sprint 2/Sprint2WPF/Sprint2WPF/Berechnungen.cs
+++ Sprint 2/Sprint2WPF/Sprint2WPF/Berechnungen.cs
@@ -19,18 +19,16 @@ namespace Sprint2WPF
         }
 
         //Volumen
-        public double getVolumen(double v_k_d, double v_k_l, double v_s_d, double v_s_l, double v_gw_d, double v_gw_l)
+        public double getVolumen(double v_k_d, double v_k_l, double v_s_d, double v_s_l)
         {
             double v_ergebnis;
             double v_ergebnis_k;
             double v_ergebnis_s;
-            double v_ergebnis_gw;
 
             v_ergebnis_k = Math.PI * (1 / 4) * Math.Pow(v_k_d, 2) * v_k_l;
             v_ergebnis_s = Math.PI * (1 / 4) * Math.Pow(v_s_d, 2) * v_s_l;
-            v_ergebnis_gw = Math.PI * (1 / 4) * Math.Pow(v_gw_d, 2) * v_gw_l;
 
-            v_ergebnis = v_ergebnis_k + v_ergebnis_s + v_ergebnis_gw;
+            v_ergebnis = v_ergebnis_k + v_ergebnis_s;
 
             return v_ergebnis;
         }
