c8f18bfda34525db36dae69b8a40ae9b0805e11a
O.Bätjer <79996046+baetjer@users.noreply.github.com>
2021-05-20 11:56:45 +0200

    Create MainWindow.xaml.cs

---
+++ Sprint 3/WPF-Anwendung/Sprint3WPF/Sprint3WPF/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sprint3WPF
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void tvi_sk_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void tvi_zy1_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void tvi_zy2_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void tvi_ss_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void cbx_gew_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void cbx_laenge_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void cbx_stkz_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
