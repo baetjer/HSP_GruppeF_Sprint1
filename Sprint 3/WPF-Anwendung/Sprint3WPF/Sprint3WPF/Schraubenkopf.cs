c4d2048fe4b8f073554a9e754a0a61743d7e59ec
O.Bätjer <79996046+baetjer@users.noreply.github.com>
2021-05-28 11:49:26 +0200

    Create Schraubenkopf.cs

---
+++ Sprint 3/WPF-Anwendung/Sprint3WPF/Sprint3WPF/Schraubenkopf.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint3WPF
+{
+    class Schraubenkopf
+    {
+        public Schraubenkopf(double wert1, double wert2, double wert3)
+        {
+            this.wert1 = wert1;
+            this.wert2 = wert2;
+            this.wert3 = wert3;
+        }
+
+        public double wert1 { get; private set; }
+        public double wert2 { get; private set; }
+        public double wert3 { get; private set; }
+    }
+}
